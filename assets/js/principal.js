0481410c9838d91caae38cef6fa3c9b6eec2c6bc
Rodrigo <rcbm539@gmail.com>
2017-08-16 20:29:47 -0300

    Imc calculado

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -1,5 +1,21 @@
-var paciente = document.querySelector("#primeiro-imc");
-console.log(paciente);
-var tdPeso = document.querySelector("paciente");
-console.log(tdPeso);
+var pessoas = document.querySelectorAll(".pessoa");
+for(var i = 0; i < pessoas.length; i++){
+    var pessoa = pessoas[i];
+        //Captura peso
+    var tdPeso = pessoa.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    //Captura Altura
+    var tdAltura = pessoa.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    //Captura IMC
+    var tdImc = pessoa.querySelector(".info-imc");
+    console.log(tdImc);
+    //Calcula IMC
+    var imc = peso / (altura * altura);
+    console.log("IMC = " + imc);
+    tdImc.textContent = imc;
+}
+
+
+
 
